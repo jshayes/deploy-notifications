141a5d97d3fa4d631b89e6e46185ba4a1eafbe89
Justin Hayes <justin@soapboxhq.com>
2017-06-28 21:48:00 -0400

    Fix an issue with the github client

--- app/GithubClient.php
+++ app/GithubClient.php
@@ -8,13 +8,13 @@ class GithubClient
 {
     public function __construct()
     {
-        $client = new Client();
-        $client->authenticate(env('GITHUB_TOKEN'), Client::AUTH_HTTP_TOKEN);
+        $this->client = new Client();
+        $this->client->authenticate(env('GITHUB_TOKEN'), Client::AUTH_HTTP_TOKEN);
     }
 
     public function getCommit($user, $repo, $commit)
     {
-        return $client->api('repo')->commits()->show($user, $repo, $commit);
+        return $this->client->api('repo')->commits()->show($user, $repo, $commit);
 
     }
 }
